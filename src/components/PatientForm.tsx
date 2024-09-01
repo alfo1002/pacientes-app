94a0ad86b0869d2da83adbbf28d11efa63ff4d0b
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-08-31 19:00:35 -0500

    add: agregar funcionalidad de resetear formulario al guardar con reset() de zustand

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -7,10 +7,11 @@ export const PatientForm = () => {
 
     //const addPatient = usePatientStore(state => state.addPatient)  //dos opciones para obtener la función addPatient
     const { addPatient } = usePatientStore()
-    const { register, handleSubmit, formState: { errors } } = useForm<DraftPatient>()
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<DraftPatient>()
 
     const registerPatient = (data: DraftPatient) => {
         addPatient(data)
+        reset()
     }
 
     return (
